4260bbb01442541cfa37e382d608a4ac7fd8f1d2
surya04101998@gmail.com <surya04101998@gmail.com>
2020-08-14 17:31:24 +0530

    test

--- ClickStar/src/test/java/com/example/demo/ClickStarApplicationTests.java
+++ ClickStar/src/test/java/com/example/demo/ClickStarApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class ClickStarApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void fakeAssert() {
+		assertEquals(1, 1);
 	}
 
 }
